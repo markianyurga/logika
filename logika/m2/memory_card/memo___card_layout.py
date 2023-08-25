969ed74dd63cec990453519d42105899724b5a64
markianyurga <140836073+markianyurga@users.noreply.github.com>
2023-08-25 15:39:58 +0300

    Add files via upload

--- logika/m2/memory_card/memo___card_layout.py
+++ logika/m2/memory_card/memo___card_layout.py
@@ -9,6 +9,73 @@ from PyQt5.QtWidgets import (
         QPushButton, QLabel, QSpinBox)
 app = QApplication([])
 
+
+btn_ok = QPushButton('відловісти')
+btn_sleep = QPushButton('відпочити')
+btn_menu = QPushButton('мнню')
+
+lb_question = QLabel('')
+
+box_minutes = QSpinBox()
+box_minutes.setValue(5)
+
+RadioGroupBox = QGroupBox('Варіанти відповідей')
+
+RadioGroup = QButtonGroup()
+
+r_bt1 = QRadioButton('')
+r_bt2 = QRadioButton('')
+r_bt3 = QRadioButton('')
+r_bt4 = QRadioButton('')
+
+RadioGroup.addButton(r_bt1)
+RadioGroup.addButton(r_bt2)
+RadioGroup.addButton(r_bt3)
+RadioGroup.addButton(r_bt4)
+
+layout_ans1 = QHBoxLayout()
+layout_ans2 = QHBoxLayout()
+layout_ans3 = QHBoxLayout()
+
+layout_ans2.addWidget(r_bt1)
+layout_ans2.addWidget(r_bt2)
+
+layout_ans3.addWidget(r_bt3)
+layout_ans3.addWidget(r_bt4)
+
+layout_ans1.addWidget(r_bt2)
+layout_ans1.addWidget(r_bt3)
+
+RadioGroupBox.setLayout(layout_ans1)
+
+AnsGroupBox = QGroupBox()
+lb_rosult = QLabel('')
+lb_correct = QLabel('')
+
+layout_res = QVBoxLayout()
+layout_res.addWidget(lb_rosult, alignment=(Qt.Alignleft | Qt.AlignTor))
+layout_res.addWidget(lb_correct, alignment=Qt.AligHCenter, stretch=2)
+
+AnsGroupBox.setLayout(layout_res)
+
+AnsGroupBox.hide()
+
+layout_card = QVBoxLayout()
+layout_line1 = QHBoxLayout()
+layout_line2 = QHBoxLayout()
+layout_line3 = QHBoxLayout()
+layout_line4 = QHBoxLayout()
+
+layout_line1.addWidget(btn_menu)
+layout_line1.addWidget(1)
+layout_line1.addWidget(btn_sleep)
+layout_line1.addWidget(box_minutes)
+
+layout_line1.addWidget(QLabel('хвилини'))
+
+layout_line2.addWiget(lb_question, alignment=(Qt.AlignHC))
+
+
 # віджети, які треба буде розмістити:
 # кнопка повернення в основне вікно 
 # кнопка прибирає вікно і повертає його після закінчення таймера
