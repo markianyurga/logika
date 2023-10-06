150825f0dccab68ae0b6255f1b4427f2a5b1bb28
markianyurga <140836073+markianyurga@users.noreply.github.com>
2023-10-06 20:08:10 +0300

    Add files via upload

--- logika/m3/u1/smart_notes/smart_notes.py
+++ logika/m3/u1/smart_notes/smart_notes.py
@@ -25,6 +25,54 @@ btn_tag_search = QPushButton('шукати за тегом')
 
 fild_txt = QTextEdit()
 
+loyout_notes = QHBoxLayout()
+col1 = QVBoxLayout()
+col2 = QVBoxLayout()
 
+loyout_notes.addLayout(col1, stretch=2)
+loyout_notes.addLayout(col2, stretch=1)
+col1.addWidget(fild_txt)
+col2.addWidget(lb_note )lst_note btn_note_cerelt btn_note_delete btn_note_sawe lb_tag fild_tag btn_tag_add btn_tag_unpin btn_tag_search)
+col2.addWidget(lst_note) btn_note_cerelt btn_note_delete btn_note_sawe lb_tag fild_tag btn_tag_add btn_tag_unpin btn_tag_search)
+col2.addWidget(btn_note_cerelt) btn_note_delete btn_note_sawe lb_tag fild_tag btn_tag_add btn_tag_unpin btn_tag_search)
+
+def add_teg():
+    key = lst_note.currentItem().text()
+    tag = field_tag.text()
+    
+    lst_tag.addItem(tag)
+    
+    saveToFile()
+
+def del_teg():
+    pass
+
+def search_teg():
+    tag = fild_tag.text()
+    
+    if 'шукати за тегом' ==btn_tag_search.text():
+        filtered_notes = {}
+        
+        for key in notes:
+            if tag in notes[key]['теги']:
+                filtered_notes[key] = notes[key]
+                
+                
+        btn_tag_search.setText("скинути пошук")  
+         
+        lst_note.clear()
+        lst_note.addItems(notes)
+        lst_tag.clear()
+        fild_txt.clear()
+    
+    
+    
+    elif 'скинути тег' == btn_tag_search.text():
+        btn_tag_search.setText('шукати тег')
+
+with open('note.json', 'r', encoding='utf8') as file:
+    notes = json.load(file)
+
+windof.setLayout(loyout_notes)
 windof.show()
 app.exec_()
