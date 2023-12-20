5a2f5e99428adc7afde8d4736102c5ca249a9f89
markianyurga <140836073+markianyurga@users.noreply.github.com>
2023-12-20 18:59:28 +0200

    Add files via upload

--- logika/m5/u1/catch_up.py
+++ logika/m5/u1/catch_up.py
@@ -10,9 +10,15 @@ window = display.set_mode([750, 550])
 
 background = transform.scale(image.load('background.png'), [750, 550])
 #створи 2 спрайти та розмісти їх на сцені
-sprite1 = transform.scale(image.load('sprite1.png'), [82, 80])
+sprite1 = transform.scale(image.load('sprite1.png'), [82, 90])
 x1 = 50
 y1 = 450
+
+sprite2 = transform.scale(image.load('sprite2.png'), [80, 80])
+x2 = 50
+y2 = 450
+
+
 #оброби подію «клік за кнопкою "Закрити вікно"»
 
 geme = True
@@ -26,6 +32,7 @@ while geme:
             geme = False
     window.blit(background, (0, 0))
     window.blit(sprite1, (x1, y1))
+    window.blit(sprite2, (x2, y2))
     keys = key.get_pressed()
     if keys[K_UP]:
         y1-=10
@@ -39,6 +46,17 @@ while geme:
     if keys[K_RIGHT]:
         x1+=10
 
+    if keys[K_w]:
+        y2-=10
+        
+    if keys[K_s]:
+        y2+=10
+
+    if keys[K_a]:
+        x2-=10
+        
+    if keys[K_d]:
+        x2+=10
 
 #K_a # вліво
 #K_w # вверх
