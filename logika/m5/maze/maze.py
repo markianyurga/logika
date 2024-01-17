91f7ed0df399ed020d9ca719353d7451c0d556f2
markianyurga <140836073+markianyurga@users.noreply.github.com>
2024-01-17 18:31:27 +0200

    Add files via upload

--- logika/m5/maze/maze.py
+++ logika/m5/maze/maze.py
@@ -16,9 +16,34 @@ class GameSprite(Sprite):
     def reset(self):
         WINDOW.blit(self.image,[self.rect.x, self.rect.y])
 
-player = GameSprite('hero.png', 450, 55, 6)
-player2 = GameSprite('treasure.png', 450, 40, 6)
-player3 = GameSprite('cyborg.png', 400, 55, 6)
+class Player(GameSprite):
+    def update(self):
+        pressed = key.get_pressed()
+        if pressed[K_UP]:
+            self.rect.y -= self.speed
+            
+        if pressed[K_DOWN]:
+            self.rect.y += self.speed
+            
+        if pressed[K_LEFT]:
+            self.rect.x -= self.speed
+            
+        if pressed[K_RIGHT]:
+            self.rect.x += self.speed
+        
+class Enemy(GameSprite):
+    direction = 'left'
+    def update(self):
+        if self.direction == 'left':
+            self.rect.x -= self.speed
+        
+        if self.direction == 'right':
+            self.rect.x += self.speed
+         
+
+player = Player('hero.png', 10, 10, 6)
+player2 = GameSprite('treasure.png', 660, 400, 6)
+player3 = Enemy('cyborg.png', 660, 311, 6)
 
 WINDOW = display.set_mode([750, 500])
 
@@ -37,6 +62,10 @@ while game:
     player.reset()
     player2.reset()
     player3.reset()
+    
+    player.update()
+    player3.update()
+    
     display.update()
     clock.tick(fps)  
     
